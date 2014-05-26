b28a273b0719e2ce253a8402a9d5dda371589bad
Eiddoel <eiddoel@gmail.com>
2014-05-27 07:32:24 +0800

    get properties

--- src/com/iman/nrms/nrmgs/GatherContainer.java
+++ src/com/iman/nrms/nrmgs/GatherContainer.java
@@ -1,12 +1,17 @@
 package com.iman.nrms.nrmgs;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Properties;
 
+import org.apache.log4j.Logger;
+
 public class GatherContainer {
+	static Logger logger = Logger.getLogger(GatherContainer.class);
+	
 	private static GatherContainer gatherContainer = new GatherContainer();
 	//保证只存在一个实例，只能由getInstance()取得，不能new出来。
 	private GatherContainer(){}
@@ -18,17 +23,22 @@ public class GatherContainer {
 	}
 	
 	public void run(){
-		try {
-			InputStreamReader is = new InputStreamReader(new FileInputStream("config/gather.cfg.properties"));
+/*		try {
+			
+			InputStreamReader is = new InputStreamReader(new FileInputStream("gather.cfg.properties"));
 			Properties porperties=new Properties();
 			porperties.load(is);
-			porperties.getProperty("isTest");
+			String isTest = porperties.getProperty("isTest");
+			logger.debug(isTest);
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}catch(IOException e1){
 			e1.printStackTrace();
-		}
+		}*/
+		File f = new File("");
+		logger.debug(""+f.getAbsolutePath());
+		logger.debug(""+System.getProperty("user.dir"));
 	}
 
 }
